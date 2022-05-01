e0ba2ca55760ffbfb25f372612f16424dce8ba5a
Sergii Fesenko <sergiifesenko@Sergiis-MacBook-Air.local>
2022-05-01 20:59:06 +0300

    simple input save to memory

--- main.go
+++ main.go
@@ -2,8 +2,40 @@ package main
 
 import "fmt"
 
+var id = 0
+
 func main() {
 
-	fmt.Println("Hello world!")
+	var userInput string
+	type Record struct {
+		Note string
+		id   int
+	}
+	var notes = make([]Record, 0)
+
+	for {
+		fmt.Printf("Enter your note:\n")
+		fmt.Scan(&userInput)
+
+		switch userInput {
+		case "/l":
+			for _, note := range notes {
+				fmt.Printf("note id: %v\nnote: %v\n", note.id, note.Note)
+				fmt.Println("----------")
+			}
+		case "/q":
+			break
+		default:
+			id = id + 1
+
+			newRecord := Record{
+				Note: userInput,
+				id:   id,
+			}
+
+			notes = append(notes, newRecord)
+		}
+
+	}
 
 }
